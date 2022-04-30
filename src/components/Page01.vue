0f96be4698984beabed6d76f6facc53749ac02a4
Daisuke <d.suke.kida@gmail.com>
2022-04-30 17:55:56 +0900

    header and side bar

---
+++ src/components/Page01.vue
@@ -0,0 +1,32 @@
+<template>
+<div>
+  <v-app-bar class="bg-cyan-darken-1 text-white">
+    <v-app-bar-nav-icon variant="text" @click="drawer = !drawer"/>
+    <v-toolbar-title>Vuetify Practice</v-toolbar-title>
+
+    <v-spacer></v-spacer>
+
+    <v-btn variant="text" icon="mdi-magnify"></v-btn>
+    <v-btn variant="text" icon="mdi-filter"></v-btn>
+    <v-btn variant="text" icon="mdi-dots-vertical"></v-btn>
+  </v-app-bar>
+
+  <v-navigation-drawer v-model="drawer" temporary>
+    <v-list :items="items"></v-list>
+  </v-navigation-drawer>
+
+</div>
+</template>
+
+<script>
+export default {
+  name: 'Page01',
+  data: () => ({
+    drawer: null,
+    items: [
+      { title:'HOME', value:'home' },
+      { title:'COMPANY', value:'company' }
+    ],
+  })
+}
+</script>

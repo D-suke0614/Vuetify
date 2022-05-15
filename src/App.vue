589c33e3ad7235baab8aac28ea99ac38155e5668
Daisuke <d.suke.kida@gmail.com>
2022-05-15 19:23:32 +0900

    commit

--- src/App.vue
+++ src/App.vue
@@ -1,6 +1,6 @@
 <template>
   <v-app>
-    <v-app-bar class="light-blue lighten-2 white--text text-sm-h4">
+    <!-- <v-app-bar class="light-blue lighten-2 white--text text-sm-h4">
       <v-app-bar-nav-icon class="white--text" variant="text" @click="drawer = !drawer"/>
       <v-toolbar-title>Vuetify Practice</v-toolbar-title>
 
@@ -9,9 +9,9 @@
       <v-btn variant="text" icon="mdi-magnify"></v-btn>
       <v-btn variant="text" icon="mdi-filter"></v-btn>
       <v-btn variant="text" icon="mdi-dots-vertical"></v-btn>
-    </v-app-bar>
+    </v-app-bar> -->
 
-    <v-navigation-drawer v-model="drawer" temporary absolute>
+    <!-- <v-navigation-drawer v-model="drawer" temporary absolute>
       <v-list-item-group v-model="group" active-class="light-blue lighten-5 text--accent-4">
           <v-list-item>
             <v-list-item-title>Foo</v-list-item-title>
@@ -29,11 +29,12 @@
             <v-list-item-title>Buzz</v-list-item-title>
           </v-list-item>
         </v-list-item-group>
-    </v-navigation-drawer>
+    </v-navigation-drawer> -->
       <!-- <Page01 /> -->
       <!-- <HelloWorld /> -->
       <v-main>
-        <Calender />
+        <!-- <Calender /> -->
+        <Axios />
       </v-main>
   </v-app>
 
@@ -43,6 +44,7 @@
 import HelloWorld from './components/HelloWorld';
 import Page01 from './components/Page01';
 import Calender from './components/Calender.vue'
+import Axios from './components/Axios-pra.vue'
 
 export default {
   name: 'App',
@@ -50,7 +52,8 @@ export default {
   components: {
     Page01,
     HelloWorld,
-    Calender
+    Calender,
+    Axios
   },
 
   data: () => ({
